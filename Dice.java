32af5a8e51b5513a79f676aefd42d1d1ea5200af
Ole Snyders <124588267+IBimsHedebe@users.noreply.github.com>
2026-04-17 13:09:36 +0200

    init dice

--- Dice.java
+++ Dice.java
@@ -4,13 +4,41 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Dice class
  * 
  * made by Ole
- * added 10.04.26
+ * added 17.04.26
  */
-public class Dice extends Dice_Screen
+public class Dice extends Actor
 {
-    public int rolledNumber;
+    public int randomNumber;
+    public GreenfootImage[] idleImages;
+    public GreenfootImage[] rollImages;
     
-    public void Roll_Number(){
-        rolledNumber = Greenfoot.getRandomNumber(6) + 1;
+    public Dice(){
+        // Anzahl Bilder
+        idleImages = new GreenfootImage[1];
+        rollImages = new GreenfootImage[6];
+        
+        // Lade Bilder
+        idleImages[1] = new GreenfootImage("dice/" + "dice" + ".png");
+        idleImages[1].scale(48, 48); // verändert die Größe beim Laden der Spielfigur
+        
+        for (int i = 0; i < rollImages.length; i++){
+            rollImages[i] = new GreenfootImage("dice/" + i + ".png");
+            rollImages[i].scale(48, 48); // verändert die Größe beim Laden der Spielfigur
+        }
+        
+        setImage(idleImages[1]);
+    }
+    
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(Dice.class)){
+            randomNumber = Greenfoot.getRandomNumber(6) + 1;
+            animate();
+        }
+        
+    }
+    
+    public void animate(){
+        
     }
 }
